fddb3eaa4963e6460b5a9556b5c0e140ef7c832e
Dell <Dell@DESKTOP-L00I9MD.local>
2019-05-22 17:40:53 +0200

    Menu positioning fixed.

--- src/actions/OverspreadAction.js
+++ src/actions/OverspreadAction.js
@@ -10,10 +10,8 @@ class OverspreadAction {
 	}
 	
 	_update() {
-		let constant = 1;//2.5
-		let screenWidth = window.innerWidth / constant;
-		let fullWidth = -5;
-		let offset = 5;
+		let screenWidth = window.innerWidth;
+		let fullWidth = -4 * this._offset;
 		let widthPrev = fullWidth;
 	    let lineGroups = [];
 	    let maxWidth = 0;
@@ -21,13 +19,15 @@ class OverspreadAction {
 		let oneLine = true;
 		
 		lineGroups.push(currentGroup);
-		
+
 		for (let entry of this._children) {
 			widthPrev = fullWidth;
 			
-	        fullWidth += entry.element.offsetWidth + offset;
+	        if (entry !== this._children[0]) {
+		        fullWidth += entry.element.offsetWidth + this._offset;
+	        }
 			
-	        if (currentGroup.length > 0 && fullWidth * constant > screenWidth) {
+	        if (currentGroup.length > 0 && fullWidth > screenWidth) {
 	        	oneLine = false;
 	        	maxWidth = Math.max(maxWidth, widthPrev);
 	        	fullWidth = entry.element.offsetWidth;
@@ -40,9 +40,8 @@ class OverspreadAction {
 	        currentGroup.push(entry);
 		};
 
-	    let y = window.innerHeight - 75;
+	    let y = 1000 / window.innerHeight + 250;
 	    this._children[0].visible = !oneLine;
-	    //this._children[0].textElement.visible(!oneLine);
 	    
 		for (let entry of this._children) {
 	        if (entry === this._children[0]) {
@@ -50,59 +49,46 @@ class OverspreadAction {
 	        }
 	        
 	        entry.visible = oneLine || !this._hiddenItems;
-	        //entry.textElement.visible(oneLine || !this._hiddenItems);
 	    };
 	    
 	    if (!oneLine && this._hiddenItems) {
-	    	this._children[0].position.x = (- screenWidth + offset) / 10;
-	    	this._children[0].position.y = y / 25;
+	    	this._children[0].position.x = (- screenWidth + this._offset);
+	    	this._children[0].position.y = y;
 	    	
 	    	return;
 	    }
 
-		maxWidth += oneLine ? 0 : (this._children[0].element.offsetWidth + offset);
+		maxWidth += oneLine ? 0 : (this._children[0].element.offsetWidth + this._offset);
 		
-		let start = oneLine ? - maxWidth * 2.25 : (- screenWidth + offset);
+		let start = oneLine ? - maxWidth / 2 : (- screenWidth + this._offset);
 
 		for (let group of lineGroups) {
 	        let iter = start;
 	        
 			for (let entry of group) {
-	            if (entry == this._children[0] && oneLine) {
+	            if (entry === this._children[0] && oneLine) {
 	            	continue;
 	            }
-	//alert(entry.positionX + ' ' + iter + ' ' + entry.positionY + ' ' + y);
-	            entry.position.x = iter / 10;//alert(iter / 10 + ' ' + (y / 25));
-	            entry.position.y = y / 25;
-	            //entry.position.x = iter;
-	            //entry.position.y = y;
-	    		iter += (entry.element.offsetWidth + offset) * 5;
+
+	            entry.position.x = iter;
+	            entry.position.y = y / 2;
+	    		iter += entry.element.offsetWidth + this._offset;
 			};
 			
-			y -= (this._children[0].element.offsetHeight - offset) * 5;
+			y -= this._children[0].element.offsetHeight - this._offset;
 		};
 		
 	}
-    
 	
 	_doStep() {
-
-		/*if (typeof this._TEST !== 'undefined')
-		for (let menuItem of this._TEST.menu.items) {
-			console.log( menuItem.element.innerHTML, menuItem.element.offsetWidth );
-		}*/
+		this._update();
 	}
 	
 	_continueCondition() {
-		this._update();
-		return false;
-		//return this._element.rotation.y != this._angle;
+		return true;
 	}
 	
 	start() {
-		//this._angle = angle - this._element.rotation.y || 0;
-		//this._radius = Math.sqrt(Math.pow(this._x - this._element.position.x, 2) + Math.pow(this._z - this._element.position.z, 2));
-		
 		if (this._task == null || !this._task.valid) {
 			this._task = this._gumaReference.animationManager.addTask(this._doStep.bind(this), this._continueCondition.bind(this));
 		}
