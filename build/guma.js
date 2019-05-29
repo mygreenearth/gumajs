0de8b3fff82c068b0f02448038c64325f0208bf9
Dell <Dell@DESKTOP-L00I9MD.local>
2019-05-29 19:29:35 +0200

    build

--- build/guma.js
+++ build/guma.js
@@ -80,6 +80,116 @@ class MoveAction {
 		}
 	}
 }
+class MoveAndResizeAction {
+	constructor(gumaReference, element, moveSpeed, resizeSpeed, rotateSpeed) {
+		this._gumaReference = gumaReference;
+		this._element = element;
+		this._maxMoveSpeed = moveSpeed || 10;
+		this._maxResizeSpeedSpeed = resizeSpeed || 10;
+		this._maxRotateSpeed = rotateSpeed || 0.04;
+		this._task = null;
+	}
+	
+	_doStep() {
+		if (this._element.position.x < this._x) {
+			this._element.position.x = Math.min(this._x, this._element.position.x + this._speedX);
+		} else if (this._element.position.x > this._x) {
+			this._element.position.x = Math.max(this._x, this._element.position.x - this._speedX);
+		}
+		
+		if (this._element.position.y < this._y) {
+			this._element.position.y = Math.min(this._y, this._element.position.y + this._speedY);
+		} else if (this._element.position.y > this._y) {
+			this._element.position.y = Math.max(this._y, this._element.position.y - this._speedY);
+		}
+		
+		if (this._element.position.z < this._z) {
+			this._element.position.z = Math.min(this._z, this._element.position.z + this._speedZ);
+		} else if (this._element.position.z > this._z) {
+			this._element.position.z = Math.max(this._z, this._element.position.z - this._speedZ);
+		}
+
+		if (this._element.width < this._width) {
+			this._element.width = 
+				Math.min(this._width, this._element.width + this._speedWidth);
+		} else if (this._element.width > this._width) {
+			this._element.width = 
+				Math.max(this._width, this._element.width - this._speedWidth);
+		}
+		
+		if (this._element.height < this._height) {
+			this._element.height = 
+				Math.min(this._height, this._element.height + this._speedHeight);
+		} else if (this._element.height > this._height) {
+			this._element.height = 
+				Math.max(this._height, this._element.height - this._speedHeight);
+		}
+		
+		this._element.rotation.x = Utils.getRotateStep(this._element.rotation.x, this._rotX, this._speedRotX);
+		this._element.rotation.y = Utils.getRotateStep(this._element.rotation.y, this._rotY, this._speedRotY);
+		this._element.rotation.z = Utils.getRotateStep(this._element.rotation.z, this._rotZ, this._speedRotZ);
+	}
+	
+	_continueCondition() {
+		return this._x != this._element.position.x
+		|| this._y != this._element.position.y
+		|| this._z != this._element.position.z
+		|| this._width != this._element.width
+		|| this._height != this._element.height
+		|| this._rotX != this._element.rotation.x
+		|| this._rotY != this._element.rotation.y
+		|| this._rotZ != this._element.rotation.z;
+	}
+	
+	_recalculateSpeeds() {
+		let xDistance = Math.abs(this._x - this._element.position.x);
+		let yDistance = Math.abs(this._y - this._element.position.y);
+		let zDistance = Math.abs(this._z - this._element.position.z);
+		let widthDistance = Math.abs(this._width - this._element.width);
+		let heightDistance = Math.abs(this._height - this._element.height);
+		let rotXDistance = Math.abs(this._rotX - this._element.rotation.x);
+		let rotYDistance = Math.abs(this._rotY - this._element.rotation.y);
+		let rotZDistance = Math.abs(this._rotZ - this._element.rotation.z);
+		
+		let xSteps = xDistance / this._maxMoveSpeed;
+		let ySteps = yDistance / this._maxMoveSpeed;
+		let zSteps = zDistance / this._maxMoveSpeed;
+		let widthSteps = widthDistance / this._maxMoveSpeed;
+		let heightSteps = heightDistance / this._maxMoveSpeed;
+		let rotXSteps = rotXDistance / this._maxRotateSpeed;
+		let rotYSteps = rotYDistance / this._maxRotateSpeed;
+		let rotZSteps = rotZDistance / this._maxRotateSpeed;
+		
+		let maxSteps = Math.ceil(Math.max(xSteps, ySteps, zSteps, widthSteps, heightSteps, rotXSteps, rotYSteps, rotZSteps));
+
+		this._speedX = xDistance / maxSteps;
+		this._speedY = yDistance / maxSteps;
+		this._speedZ = zDistance / maxSteps;
+		this._speedWidth = widthDistance / maxSteps;
+		this._speedHeight = heightDistance / maxSteps;
+		this._speedRotX = rotXDistance / maxSteps;
+		this._speedRotY = rotYDistance / maxSteps;
+		this._speedRotZ = rotZDistance / maxSteps;
+	}
+	
+	start(x, y, z, width, height, rotX, rotY, rotZ) {
+		this._x = x || this._element.position.x;
+		this._y = y || this._element.position.y;
+		this._z = z || this._element.position.z;
+		this._width = width || this._element.width;
+		this._height = height || this._element.height;
+		this._rotX = rotX || this._element.rotation.x;
+		this._rotY = rotY || this._element.rotation.y;
+		this._rotZ = rotZ || this._element.rotation.z;
+		
+		this._recalculateSpeeds();
+		
+		if (this._task == null || !this._task.valid) {
+			this._task = this._gumaReference.animationManager.addTask(this._doStep.bind(this), this._continueCondition.bind(this));
+		}
+	}
+}
+
 class MoveArcAction {
 	constructor(gumaReference, element, rotateSpeed) {
 		this._gumaReference = gumaReference;
@@ -381,6 +491,7 @@ class GumaPage extends THREE.CSS3DObject {
 		this.rotation.x = rotX || 0;
 		this.rotation.y = rotY || 0;
 		this.rotation.z = rotZ || 0;
+		this.inFront = false;
 		
 		this.updateSize();
 		
@@ -402,6 +513,56 @@ class GumaPage extends THREE.CSS3DObject {
 	
 	rotate(rotX, rotY, rotZ) {
 		 this.moveTo(this.position.x, this.position.y, this.position.z, rotX, rotY, rotZ);
+	}	
+	
+	popup() {
+		this.setCollapsedCoors();
+		
+		if (this.moveAndResizeAction == null) {
+			this.moveAndResizeAction = new MoveAndResizeAction(this._gumaReference, this);
+		}
+		
+		this.moveAndResizeAction.start(-20, -25, this.position.z + 1200, 800, 600);
+		this.inFront = true;
+	}
+	
+	collapse() {
+		if (this.moveAndResizeAction == null) {
+			this.moveAndResizeAction = new MoveAndResizeAction(this._gumaReference, this);
+		}
+		
+		this.moveAndResizeAction.start(this.colX, this.colY, this.colZ, this.colWidth, this.colHeight,
+				this.colRotX, this.colRotY, this.colRotZ);
+		this.inFront = false;
+	}
+
+	setCollapsedCoors(x, y, z, width, height, rotX, rotY, rotZ) {
+		this.colX = x || this.position.x;
+		this.colY = y || this.position.y;
+		this.colZ = z || this.position.z;
+		this.colWidth = width || this._width;
+		this.colHeight = height || this._height;
+		this.colRotX = rotX || this.rotation.x;
+		this.colRotY = rotY || this.rotation.y;
+		this.colRotZ = rotZ || this.rotation.z;
+	}
+	
+	get width() {
+		return this._width;
+	}
+	
+	set width(width) {
+		this._width = width;
+		this.element.style.width = this._width + 'px';
+	}
+	
+	get height() {
+		return this._height;
+	}
+	
+	set height(height) {
+		this._height = height;
+		this.element.style.height = this._height + 'px';
 	}
 }
 
@@ -599,6 +760,7 @@ class WindowsSet extends THREE.Group {
 		let yIter = 350;
 		
 		this._pages = []; //TODO Three.Group???
+		this._frontPage = null;
 		
 		let pageActions = [];
 		let colIndex = 0;
@@ -610,7 +772,7 @@ class WindowsSet extends THREE.Group {
 			//let page = new GumaPage(this._gumaReference, pageContents[i], this._pageWidth, this._pageHeight, this.position.x, this.position.y, this.position.z);
 			page.moveTo(this.position.x + x, this.position.y + yIter);
 			
-			pageActions.push(this._pageClick(page));
+			pageActions.push(this._pageClick(page, this));
 			page.element.onclick = pageActions[i];
 			
 			//this.add(page);
@@ -625,21 +787,22 @@ class WindowsSet extends THREE.Group {
 			}
 		}
 
-		this._menu = new GumaMenu(gumaReference, pageNames, /*pages(),*/ pageActions, x, y, z + 1600);
+		this._menu = new GumaMenu(gumaReference, pageNames, /*pages(),*/ pageActions, x, y, z + 1670);
 	}
 	
-	_pageClick(page) {
+	_pageClick(page, caller) {
 		return () => {
-			this.popupPage(page);
+			if (!page.inFront) {
+				if (caller._frontPage != null) {
+					caller._frontPage.collapse();
+				}
+				
+				page.popup();
+				caller._frontPage = page;
+			} else {
+				page.collapse();
+			}
 		};
-	}
-	
-	popupPage(page) {
-		if (this.rotateAction == null) {
-			this.rotateAction = new RotateAction(this._gumaReference, this);
-		}
-		
-		this.rotateAction.start(angle);
 	}
 	
 	get pages() {
@@ -685,5 +848,9 @@ class Utils {
 		}
 		
 		return Utils.trimAngle(angleFrom + Utils.getRotateDirection(angleFrom, angleTo) * speed);
+	}
+	
+	static getStylePropValue(prop) {
+		return parseInt(prop, 10);
 	}
 }
